ed76b2883cf8b9ed0df7f06f1d2f9d8e807fb735
garzen31 <garzen31@yahoo.com>
2015-04-16 14:15:43 -0400

    Destroy Fixed Joint Script
    C# code for destroyFixedJoint

---
+++ Destroy Fixed Joint Script/destroyFixedJoint.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyFixedJoint : MonoBehaviour {
+	//to add Tag look at inspector and go to Tag -> Add Tag
+	//Tags used: Sphere_1, Sphere_2, rope_1, rope_2 
+	void OnTriggerEnter(Collider activator){
+		Debug.Log ("something entered the trigger!!! omg");
+		activator.GetComponent<Rigidbody>();
+		if (activator.gameObject.tag.Equals("Sphere_1")){
+			Destroy(GameObject.FindWithTag("rope_1"));
+			Destroy (this.gameObject); //self - destruct so this barrier is gone
+		}
+		if (activator.gameObject.tag.Equals("Sphere_2")){
+			Destroy(GameObject.FindWithTag("rope_2"));
+			Destroy (this.gameObject); //self - destruct so this barrier is gone
+		}
+	}
+}
