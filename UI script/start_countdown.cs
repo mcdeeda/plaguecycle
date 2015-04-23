ab426d5f2b9992d41ca743fc8656ace0a1bc241f
mcdeeda <emcde94@gmail.com>
2015-04-23 13:32:33 -0400

    updated countdown

--- UI script/start_countdown.cs
+++ UI script/start_countdown.cs
@@ -1,31 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-using UnityEngine.YieldInstruction;
 
-public class button_press_highlight : MonoBehaviour {
+public class start_countdown : MonoBehaviour {
 	
 	public Text count_1;
 	public Text count_2;
 	public Text count_3;
-	public KeyCode keyToPress;
+	float gameTime = 0;
 	
 	// Use this for initialization
 	void Start () {
 		count_3.color = Color(256,256,256,1);
-		yield WaitforSeconds(1);
-		count_3.color = Color(256,256,256,0);
-		count_2.color = Color(256,256,256,1);
-		yield WaitforSeconds(1);
-		count_2.color = Color(256,256,256,0);
-		count_1.color = Color(256,256,256,1);
-		yield WaitforSeconds(1);
-		count_1.color = Color(256,256,256,0);
+		if(gameTime >= 1) {
+			count_3.color = Color(256,256,256,0);
+			count_2.color = Color(256,256,256,1);
+		}
 
+		if (gameTime >= 2) {
+			count_2.color = Color(256,256,256,0);
+			count_1.color = Color(256,256,256,1);
+		}
+
+		if (gameTime >=3) {
+			count_1.color = Color(256,256,256,0);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (gameTime <=5) {
+			gameTime += Time.deltaTime;
+		}
 
 	}
 }
