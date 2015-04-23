71c3a38dced7ca0529b0a4fe48512f6c547e7732
garzen31 <garzen31@yahoo.com>
2015-04-23 13:53:27 -0400

    destroyText Scripts

---
+++ destroyText Scripts/destroyText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyText : MonoBehaviour {
+
+	float gameTime;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		gameTime += Time.deltaTime;
+		if( gameTime >= 1){
+			Destroy(GameObject.FindWithTag("text_1"));
+		}
+	}
+}
---
+++ destroyText Scripts/destroyTextTwo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyText : MonoBehaviour {
+	
+	float gameTime;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		gameTime += Time.deltaTime;
+		if( gameTime >= 2){
+			Destroy(GameObject.FindWithTag("text_2"));
+		}
+	}
+}
