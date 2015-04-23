904974b4dffd9c76dfc097f56b7e950677918a31
garzen31 <garzen31@yahoo.com>
2015-04-23 13:21:55 -0400

    platformMove Script

---
+++ platformMove Script/platformMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class platformMove : MonoBehaviour {
+
+	float gameTime;
+
+	// Update is called once per frame
+	void Update () {
+
+		gameTime += Time.deltaTime;
+
+		//if (Input.GetKey (KeyCode.A) ) {
+		if( gameTime >= 3){
+			transform.position += new Vector3( -10f, 0f, 0f );
+		}
+	}
+}
