7587b6fd654881852468b1691c92e56bd2cd1d81
mcdeeda <emcde94@gmail.com>
2015-04-23 12:45:25 -0400

    added ui script

---
+++ UI script/button_press_highlight.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class button_press_highlight : MonoBehaviour {
+	
+	public Text textObject;
+	public KeyCode keyToPress;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(keyToPress)) {
+			textObject.color = Color.red;
+
+		}
+		else {
+			textObject.color = Color.black;
+		}
+	}
+}
