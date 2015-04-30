aecee2aef72287e25fa02996fb49e470cb681fe5
garzen31 <garzen31@yahoo.com>
2015-04-30 13:47:52 -0400

    Animation Scripts Updated

--- Animation Scripts/unicycleAnimation.cs
+++ Animation Scripts/unicycleAnimation.cs
@@ -22,3 +22,4 @@ public class unicycleAnimation : MonoBehaviour {
 			speed = -1;
 		}	
 	}
+}
--- Animation Scripts/unicycleAnimationPlayerTwo.cs
+++ Animation Scripts/unicycleAnimationPlayerTwo.cs
@@ -22,3 +22,4 @@ public class unicycleAnimationPlayerTwo : MonoBehaviour {
 			speed = 1;
 		}	
 	}
+}
