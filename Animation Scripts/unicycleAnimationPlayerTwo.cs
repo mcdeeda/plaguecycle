b45f2fd276a8f7bcd8278409155049c6dd5da54a
garzen31 <garzen31@yahoo.com>
2015-04-30 13:37:13 -0400

    Animation Scripts

---
+++ Animation Scripts/unicycleAnimationPlayerTwo.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class unicycleAnimationPlayerTwo : MonoBehaviour {
+
+	public float speed;
+	public Animator animator;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		animator.SetFloat("speed", speed);
+		if( Input.GetKeyDown (KeyCode.P)){  
+			speed = -1;
+			
+		}
+		if( Input.GetKeyDown (KeyCode.O)){
+			speed = 1;
+		}	
+	}
